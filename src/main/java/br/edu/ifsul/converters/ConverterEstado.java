9c2b4c9822c0abca8a93d19603ef793113d34507
Jorge <Jorge@JorgeWindows>
2022-05-10 16:29:25 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/edu/ifsul/converters/ConverterEstado.java
@@ -0,0 +1,43 @@
+package br.edu.ifsul.converters;
+
+import br.edu.ifsul.modelo.Estado;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Prof. Me. Jorge Luis Boeira Bavaresco
+ * @email jorgebavaresco@ifsul.edu.br
+ * @organization IFSUL - Campus Passo Fundo
+ */
+@Named(value = "converterEstado")
+@RequestScoped
+public class ConverterEstado implements Serializable, Converter {
+
+    @PersistenceContext(unitName = "TimesFutebolWebPU")
+    private EntityManager em;
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        if (string == null || string.equals("Selecione um registro")) {
+            return null;
+        }
+        return em.find(Estado.class, Integer.parseInt(string));
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if (o == null) {
+            return null;
+        }
+        Estado obj = (Estado) o;
+        return obj.getId().toString();
+    }
+
+}
